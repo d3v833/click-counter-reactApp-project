d1b5ccc83fa61f3c5c0dae9482903c02e51cc341
d3v833 <d3v833@gmail.com>
2019-06-05 09:43:32 +0800

    added challenge solutions

--- src/App.js
+++ src/App.js
@@ -7,17 +7,43 @@ class App extends Component {
 
 		this.state = {
 			counter: 0,
+			error: false,
 		};
+
+		this.decrementCounter = this.decrementCounter.bind(this);
+		this.incrementCounter = this.incrementCounter.bind(this);
 	}
+
+	decrementCounter() {
+		if (this.state.counter === 0) {
+			this.setState({ error: true });
+		} else {
+			this.setState({ counter: this.state.counter - 1 });
+		}
+	}
+
+	incrementCounter() {
+		if (this.state.error) {
+			this.setState({ error: false });
+		}
+		this.setState({ counter: this.state.counter + 1 });
+	}
+
 	render() {
+		// determine whether error is hidden based on state
+		const errorClass = this.state.error ? '' : 'hidden';
+
 		return (
 			<div data-test="component-app">
-				<h1 data-test="counter-display"> The counter is currently {this.state.counter}</h1>
-				<button
-					data-test="increment-button"
-					onClick={() => this.setState({ counter: this.state.counter + 1 })}
-				>
+				<h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+				<div data-test="error-message" className={`error ${errorClass}`}>
+					The counter cannot go below 0
+				</div>
+				<button data-test="increment-button" onClick={this.incrementCounter}>
 					Increment counter
+				</button>
+				<button data-test="decrement-button" onClick={this.decrementCounter}>
+					Decrement counter
 				</button>
 			</div>
 		);
