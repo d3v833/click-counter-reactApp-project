a234ef565e39e8abc9e926e01e2b7576392ed94a
d3v833 <d3v833@gmail.com>
2019-06-05 09:47:20 +0800

    updating styles

--- src/App.js
+++ src/App.js
@@ -35,15 +35,15 @@ class App extends Component {
 
 		return (
 			<div data-test="component-app">
-				<h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+				<h1 data-test="counter-display">{this.state.counter}</h1>
 				<div data-test="error-message" className={`error ${errorClass}`}>
 					The counter cannot go below 0
 				</div>
-				<button data-test="increment-button" onClick={this.incrementCounter}>
-					Increment counter
+				<button className="addBtn" data-test="increment-button" onClick={this.incrementCounter}>
+					+
 				</button>
-				<button data-test="decrement-button" onClick={this.decrementCounter}>
-					Decrement counter
+				<button className="minusBtn" data-test="decrement-button" onClick={this.decrementCounter}>
+					-
 				</button>
 			</div>
 		);
